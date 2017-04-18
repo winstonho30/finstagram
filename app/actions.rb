7049ac1fce9e5302b0632487a2bc8612d609f6b7
Winston Ho <winstonn.ho@gmail.com>
2017-04-18 03:01:46 +0000

    april week 5 day 2 update

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,58 @@
-get '/' do
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
 
+get '/posts/new' do
+    @post = Post.new
+    erb(:"posts/new")
+end
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    
+    @post = Post.new({ photo_url: photo_url, user_id: current_user.id })
+    
+    if @post.save
+        redirect(to('/'))
+    else
+        erb(:"posts/new")
+    end
+end
+
+get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb(:"posts/show")
+end
+
+get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
+end
 
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+
+        if user && user.password == password
+            session[:user_id] = user.id
+            redirect to('/')
+        else
+            @error_message = "Nothing matches. Login failed"
+            erb(:login)
+        end
 end
 
 get '/signup' do 
@@ -19,14 +69,8 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
     
     if @user.save
-    
-        "User #{username} saved the world!"
-    
-else
-    
-    erb(:signup)
-    
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
 end
-
-end
-
